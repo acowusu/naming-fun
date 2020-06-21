5f7edd23035814def21875ff2ffaa42e6e66b1d0
alexo <alexander.c.owusu@gmail.com>
2020-06-21 19:16:08 +0100

    stylistic changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo1.svg" height="100px">
+    <HelloWorld />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import sassStyles from './main.module.sass'
 
 export default {
   name: 'App',
@@ -15,9 +16,20 @@ export default {
   }
 }
 </script>
-
+<style lang="sass" >
+body
+  background:blue
+  
+</style>
 <style>
+body, html{
+  /* background: #7efff5; */
+}
+img{
+  margin: 1em
+}
 #app {
+  /* background: #7efff5; */
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
